78e3c08a28df2f06909f843ee9978f5d20d10e65
Hareesh108 <hareeshbhittam@gmail.com>
2024-05-08 10:05:59 +0530

    Added less style code

---
+++ give-style/css-preprocessors/less/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./styles.less";
+
+function App() {
+  return (
+    <div className="main-div">
+      <h1>Hy, there!!</h1>
+      <p className="sub-heading">how are you?</p>
+    </div>
+  );
+}
+
+export default App;
