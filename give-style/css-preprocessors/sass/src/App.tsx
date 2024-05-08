c8d22e798e2f323f6b478e76fde96720df30a138
Hareesh108 <hareeshbhittam@gmail.com>
2024-05-08 09:52:14 +0530

    Added sass style code

---
+++ give-style/css-preprocessors/sass/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import './styles.sass';
+
+function App() {
+  return (
+    <div className="main-div">
+      <h1 >Hy, there!!</h1>
+      <p className="sub-heading">how are you?</p>
+    </div>
+  );
+}
+
+export default App;
