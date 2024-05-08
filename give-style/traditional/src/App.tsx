e918b7c31338f4be5e86fb9bc3207bebc4729776
Hareesh108 <hareeshbhittam@gmail.com>
2024-05-08 09:07:05 +0530

    Added traditinal style code

---
+++ give-style/traditional/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 id="my_name" >Hareesh Bhittam</h1>
+      <p className="about">I am from India</p>
+    </>
+  );
+}
+
+export default App;
